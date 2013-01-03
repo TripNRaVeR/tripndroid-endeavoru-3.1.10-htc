af82322640d414a2003ac89b30f5e3a56a7baa60
TripNRaVeR <noerialexeew@live.nl>
2013-01-03 02:38:03 +0100

    cpu-tegra: fixup for AP33 cpu

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -2119,12 +2119,20 @@ struct early_suspend tegra_cpufreq_powersave_early_suspender;
 struct early_suspend tegra_cpufreq_performance_early_suspender;
 static struct pm_qos_request_list boost_cpu_freq_req;
 static struct pm_qos_request_list cap_cpu_freq_req;
+#ifdef CONFIG_TEGRA_CPU_AP33
+#define BOOST_CPU_FREQ_MIN 1500000
+#else
 #define BOOST_CPU_FREQ_MIN 1700000
+#endif
 #define CAP_CPU_FREQ_MAX 640000
 #endif
 static int enter_early_suspend = 0;
 static int perf_early_suspend = 0;
+#ifdef CONFIG_TEGRA_CPU_AP33
+static int CAP_CPU_FREQ_TARGET = 1500000;
+#else
 static int CAP_CPU_FREQ_TARGET = 1700000;
+#endif
 
 static int tegra_pm_notify(struct notifier_block *nb, unsigned long event,
 	void *dummy)
@@ -2256,7 +2264,11 @@ static int tegra_cpufreq_resume(struct cpufreq_policy *policy)
 {
 	/*if it's a power key wakeup, uncap the cpu powersave mode for future boost*/
 	if (wake_reason_resume == 0x80)
+#ifdef CONFIG_TEGRA_CPU_AP33
+		policy->max = 1500000;
+#else
 		policy->max = 1700000;
+#endif
 	return 0;
 }
 
