9968e028aa20374325f9087ecbdbfd5dbdf1b429
TripNRaVeR <noerialexeew@live.nl>
2013-02-18 21:05:01 +0100

    board-endeavoru-panel: fix warning passing argument 5 of 'setup_timer_key' makes integer from pointer without a cast

--- arch/arm/mach-tegra/board-endeavoru-panel.c
+++ arch/arm/mach-tegra/board-endeavoru-panel.c
@@ -4513,7 +4513,7 @@ int __init endeavor_panel_init(void)
 				ARRAY_SIZE(endeavor_bl_devices));
 	INIT_WORK(&bkl_work, bkl_do_work);
 	bkl_wq = create_workqueue("bkl_wq");
-	setup_timer(&bkl_timer, bkl_update, NULL);
+	setup_timer(&bkl_timer, bkl_update, 0);
 
 failed:
 	DISP_INFO_OUT();
