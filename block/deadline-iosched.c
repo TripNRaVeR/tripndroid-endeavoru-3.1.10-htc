6366191b42e643f5976469c6498aaec54202370b
TripNRaVeR <noerialexeew@live.nl>
2013-06-03 02:58:46 +0200

    block: update deadline scheduler tweaks

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,9 +17,9 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = HZ / 6;  /* max time before a read is submitted. */
-static const int write_expire = 1 * HZ; /* ditto for writes, these limits are SOFT! */
-static const int writes_starved = 2;    /* max times reads can starve a write */
+static const int read_expire = HZ / 3;  /* max time before a read is submitted. */
+static const int write_expire = 8 * HZ; /* ditto for writes, these limits are SOFT! */
+static const int writes_starved = 4;    /* max times reads can starve a write */
 static const int fifo_batch = 16;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
